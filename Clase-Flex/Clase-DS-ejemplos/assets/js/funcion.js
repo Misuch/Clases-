2851a511d4bf4a57ed115d062ce59528f1234233
Misuch <abujaico@estudiantes.crackthecode.la>
2021-12-17 08:28:25 -0600

    Probando la subida en mi rama

--- Clase-Flex/Clase-DS-ejemplos/assets/js/funcion.js
+++ Clase-Flex/Clase-DS-ejemplos/assets/js/funcion.js
@@ -26,3 +26,51 @@ class Pila{
         console.log(this.pila);
     }
 }
+
+class Cola{
+    //Definir constructor, que será el representante de este objeto
+    constructor(){
+        this.cola = [];
+    }
+    //METODOS DEL OBJETO "PILA"
+    //Agregar un elemento -> necesito saber que elemento agregar
+    add(element){
+        this.cola.push(element);
+        return this.cola;
+    }
+    //Eliminar un elemento
+    delete(){
+        return this.cola.shift();
+    }
+    //Saber el tamaño de la pila (la cantidad de elementos)
+    getSize(){
+        return this.cola.length;
+    }
+    //Leer un elemento (el último en haberse agregado)
+    getElement(){
+        return this.cola[0];
+    }
+    //Leer todos los elementos
+    getElements(){
+        console.log(this.cola);
+    }
+}
+
+class Arbol{
+    constructor(node){
+        this.node = node;
+        this.descendents = [];
+    }
+
+    addNode(node){
+        this.descendents.push(node);
+        return this.descendents;
+    }
+
+    addElement(element){
+        this.node=element;
+        return this.node;
+    }
+}
+
+
