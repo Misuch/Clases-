43240a7e7669b5a4663e083e6b9db25383884c15
Misuch <abujaico@estudiantes.crackthecode.la>
2022-01-02 11:04:28 -0600

    Subiendo clases

---
+++ Clase-Flex/Refuerzo-js/assets/js/function.js
@@ -0,0 +1,8 @@
+//Red social -> Publicaciones
+
+let reacciones_disponibles = ["Me gusta", "Me divierte", "Me entristece"];
+let coorreo_electronico = "usuario-78@gmail.com";
+let clave = "usuario45244";
+let url_foto_perfil = "assets.mysocialnetwork.com?=id/assets/img/perfil.png";
+let nombre = "Juan Gutierrez";
+let edad = 20;
